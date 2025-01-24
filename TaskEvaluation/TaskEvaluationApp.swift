7ab63448910d97bcb206484e2ac42fb383af428d
eveliniosca <86342068+eveliniosca@users.noreply.github.com>
2025-01-24 18:59:51 +0530

    Initial Commit

---
+++ TaskEvaluation/TaskEvaluationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskEvaluationApp.swift
+//  TaskEvaluation
+//
+//  Created by Evelin on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskEvaluationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
